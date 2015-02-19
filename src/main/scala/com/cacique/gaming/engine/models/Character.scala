75c9a1bfb57b1a7581fc73efd372f2f79a4774ec
Carlo Scarioni <carlo.scarioni@simplybusiness.co.uk>
2015-02-19 17:34:57 +0000

    moved move code to character

--- src/main/scala/com/cacique/gaming/engine/models/Character.scala
+++ src/main/scala/com/cacique/gaming/engine/models/Character.scala
@@ -15,6 +15,35 @@ class Character(name: String) extends WithDrawable{
   protected var rotation = 0.0
   protected val timeUpdate = 0.05
 
+
+  def moveForward = {
+    position = position.add(velocity.mapMultiply(timeUpdate))
+  }
+
+  def moveBackward = {
+    position = position.subtract(velocity.mapMultiply(timeUpdate))
+  }
+
+  def turnLeft = {
+    val sinRadians = Math.sin(0.1)
+    val cosRadians = Math.cos(0.1)
+
+    val velocityX = velocity.getEntry(0) * cosRadians - velocity.getEntry(1) * sinRadians
+    val velocityY = velocity.getEntry(0) * sinRadians + velocity.getEntry(1) * cosRadians
+    velocity = new ArrayRealVector(Array[Double](velocityX, velocityY), false)
+    position = position.add(velocity.mapMultiply(timeUpdate))
+  }
+
+  def turnRight = {
+    val sinRadians = Math.sin(-0.1)
+    val cosRadians = Math.cos(-0.1)
+
+    val velocityX = velocity.getEntry(0) * cosRadians - velocity.getEntry(1) * sinRadians
+    val velocityY = velocity.getEntry(0) * sinRadians + velocity.getEntry(1) * cosRadians
+    velocity = new ArrayRealVector(Array[Double](velocityX, velocityY), false)
+    position = position.add(velocity.mapMultiply(timeUpdate))
+  }
+
   override def drawable: Drawable = {
     new Sprite(name, position)
   }
--- src/main/scala/com/cacique/gaming/engine/models/PlayerCharacter.scala
+++ src/main/scala/com/cacique/gaming/engine/models/PlayerCharacter.scala
@@ -9,36 +9,4 @@ import org.apache.commons.math3.linear.{ArrayRealVector}
  */
 class PlayerCharacter(name: String) extends Character(name) {
 
-  def moveForward = {
-    position = position.add(velocity.mapMultiply(timeUpdate))
-  }
-
-  def moveBackward = {
-    position = position.subtract(velocity.mapMultiply(timeUpdate))
-  }
-
-  def turnLeft = {
-    val sinRadians = Math.sin(0.1)
-    val cosRadians = Math.cos(0.1)
-
-    val velocityX = velocity.getEntry(0) * cosRadians - velocity.getEntry(1) * sinRadians
-    val velocityY = velocity.getEntry(0) * sinRadians + velocity.getEntry(1) * cosRadians
-    velocity = new ArrayRealVector(Array[Double](velocityX, velocityY), false)
-    position = position.add(velocity.mapMultiply(timeUpdate))
-  }
-
-  def turnRight = {
-    val sinRadians = Math.sin(-0.1)
-    val cosRadians = Math.cos(-0.1)
-
-    val velocityX = velocity.getEntry(0) * cosRadians - velocity.getEntry(1) * sinRadians
-    val velocityY = velocity.getEntry(0) * sinRadians + velocity.getEntry(1) * cosRadians
-    velocity = new ArrayRealVector(Array[Double](velocityX, velocityY), false)
-    position = position.add(velocity.mapMultiply(timeUpdate))
-  }
-
-
-  override def drawable: Drawable = {
-    new Sprite("player", position)
-  }
 }
