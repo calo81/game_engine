8301fa946fb7bc4cc36597eb6c60775c52312aa6
Carlo Scarioni <carlo.scarioni@simplybusiness.co.uk>
2015-02-20 10:55:59 +0000

    starting adding the seeking behaviour

--- src/main/scala/com/cacique/gaming/engine/models/Enemy.scala
+++ src/main/scala/com/cacique/gaming/engine/models/Enemy.scala
@@ -1,13 +1,14 @@
 package com.cacique.gaming.engine.models
 
+import com.cacique.gaming.engine.models.behaviour.ai.movement.Seeker
 import com.cacique.gaming.engine.models.features.Position
 
 /**
  * Created by cscarion on 19/02/15.
  */
-class Enemy(name: String) extends Character(name){
+class Enemy(name: String) extends Character(name) with Seeker{
   def playerChangedPosition(playerPosition: Position): Unit = {
-
+    seek(playerPosition)
   }
 
 }
---
+++ src/main/scala/com/cacique/gaming/engine/models/behaviour/ai/movement/Seeker.scala
@@ -0,0 +1,12 @@
+package com.cacique.gaming.engine.models.behaviour.ai.movement
+
+import com.cacique.gaming.engine.models.features.Position
+
+/**
+ * Created by cscarion on 20/02/15.
+ */
+trait Seeker {
+  def seek(target: Position) = {
+
+  }
+}
