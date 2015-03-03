7d8c18248e3941ee9454a7ea9618f862199e797c
Carlo Scarioni <carlo.scarioni@simplybusiness.co.uk>
2015-03-03 10:18:27 +0000

    enemy stops at shooting distance

--- src/main/scala/com/cacique/gaming/engine/models/behaviour/ai/movement/Seeker.scala
+++ src/main/scala/com/cacique/gaming/engine/models/behaviour/ai/movement/Seeker.scala
@@ -8,6 +8,18 @@ import org.apache.commons.math3.linear.RealVector
  * Created by cscarion on 20/02/15.
  */
 
+trait StopCondition {
+  def reached: Boolean
+}
+
+case class ShootingDistance(selfPosition: Position, targetPosition: Position) extends StopCondition {
+  val shootingDistance = 10.0
+
+  override def reached: Boolean = {
+    selfPosition.vector.getDistance(targetPosition) <= shootingDistance
+  }
+}
+
 trait Seeker {
 
   protected var position: Position
@@ -16,8 +28,10 @@ trait Seeker {
   protected var maxSpeed: Double
 
   def seek(targetPosition: Position) = {
-    velocity = targetPosition.subtract(position)
-    velocity = velocity.normalize
-    position = position.add(velocity.mapMultiply(0.1))
+    if (!ShootingDistance(position, targetPosition).reached) {
+      velocity = targetPosition.subtract(position)
+      velocity = velocity.normalize
+      position = position.add(velocity.mapMultiply(0.1))
+    }
   }
 }
